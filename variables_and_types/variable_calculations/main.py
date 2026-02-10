11ee2f2256a995eb8caea0a628e49ee67652747a
fernandolegaria07-glitch <>
2026-02-10 01:23:58 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,8 @@
+item_name = "Soda" #le estamos asignando una cadena en este caso es "soda"
+item_price = 6.99
+purchase_quantity = 5
+
+total_cost = item_price * purchase_quantity
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
